e4e6378459ea2e6b5a229141d3ee8328b62c376d
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-27 17:24:03 +0600

    creating second screen from scratch using code

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heghtLabel: UILabel!
+    @IBOutlet weak var weightLable: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    let initialHeight = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func HeightSlider(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heghtLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLable.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculatePresed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight /  (height * height)
+        print(bmi)
+        
+//        displaying or navigation to next screen
+        let secondVC = SecondViewController()
+        secondVC.bmiValue =  String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
+    }
 }
 
